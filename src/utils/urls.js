ec457f9fec61a0d866ed584d11ac315f2d61ca08
fither <onur.hamarattt@gmail.com>
2021-11-18 09:04:43 +0300

    it wasn't need to specify port :/

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,6 +1,6 @@
 const apiURL = 
   process.env.NODE_ENV === 'production' ? 
-  'https://panteon-server.herokuapp.com:55021' :
+  'https://panteon-server.herokuapp.com' :
   'http://localhost:5000';
 
 export default apiURL;
